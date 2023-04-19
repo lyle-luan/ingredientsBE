eab046df0b91263460b602fecb3f852ef22f9f9e
Aaron <jiwu.luan@shopee.com>
2023-04-19 10:53:20 +0800

    nr

--- ingredients.py
+++ ingredients.py
@@ -1,89 +1,138 @@
 from flask import Flask, request, jsonify
-from requests import requests, exceptions
+import requests
+from requests import exceptions
 import os
 import openai
 import time
 
-app = Flask(__name__)
+
+class OpenAI:
+    api_key = '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'
+    prompt = '提取下面文字中的食品配料表，并分析每种配料对人体是否健康，并给出食用建议: {}'
+
+    def __init__(self):
+        openai.api_key = OpenAI.api_key
+
+    def ask(self, ingredients: str) -> str:
+        response = openai.Completion.create(
+            engine='text-davinci-003',
+            prompt=OpenAI.prompt.format(ingredients),
+            max_tokens=4096,
+            n=1,
+            stop=None,
+            temperature=0.3).choices
+        result = ''
+        for item in response:
+            result += item.text
+        return result
+
+
+class WxMini:
+    app_id = 'wx4226b6f08dfba65a'
+    app_secret = '67d2ef55944c47c92010300014b711f4'
+    token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + \
+                app_id + '&secret=' + app_secret
+    ocr_url = 'https://api.weixin.qq.com/cv/ocr/comm?access_token={}&img_url={}'
+    max_http_retry = 3
+    max_ocr_retry = 2
+
+    def __init__(self):
+        self.access_token = ''
+        self.access_token_expires_timestamp_s = time.time()
+        self.count_http_retry = 0
+
+    def __get_token(self):
+        now = time.time()
+        if now < self.access_token_expires_timestamp_s:
+            return 0, '', self.access_token
+
+        try:
+            response = requests.get(self.token_url, timeout=1)
+            response.raise_for_status()
+        except exceptions.Timeout as e:
+            self.count_http_retry += 1
+            if self.count_http_retry <= WxMini.max_http_retry:
+                return self.__get_token()
+            else:
+                self.count_http_retry = 0
+                return 1, str(e.message), ''
+        except exceptions.HTTPError as e:
+            self.count_http_retry = 0
+            return 2, str(e.message), ''
+        else:
+            self.count_http_retry = 0
+            result = response.json()
+            self.access_token = result['access_token']
+            self.access_token_expires_timestamp_s = now + result['expires_in']
+            return 0, '', self.access_token
+
+    def get_ocr(self, img_url):
+        self.__get_token()
+        wx_url = WxMini.ocr_url.format(self.access_token, img_url)
+        try:
+            response = requests.post(wx_url, timeout=1)
+            response.raise_for_status()
+        except exceptions.Timeout as e:
+            self.count_http_retry += 1
+            if self.count_http_retry <= WxMini.max_ocr_retry:
+                return self.get_ocr(img_url)
+            else:
+                self.count_http_retry = 0
+                return 1, str(e.message), ''
+        except exceptions.HTTPError as e:
+            self.count_http_retry = 0
+            return 2, str(e.message), ''
+        else:
+            self.count_http_retry = 0
+            result = response.json()
+            if result['errcode'] != 0:
+                return 3, 'wx errcode: ' + result['errcode'] + ', wx errmsg: ' + result['errmsg'], ''
+
+            ocr_result = ''
+            items = result['items']
+            for item in items:
+                ocr_result += item['text'] + ' '
+
+            return 0, '', ocr_result
+
+
 UPLOAD_FOLDER = '/var/www/newtype.top/images/'
-wx_access_token = ''
-wx_access_token_expires_at = ''
+app = Flask(__name__)
+wx = WxMini()
+gpt = OpenAI()
 
-def get_token():
-    appId = 'wx4226b6f08dfba65a'
-    appSerect = '67d2ef55944c47c92010300014b711f4'
-    url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+appId+'&secret='+appSerect
-
-    response = requests.get(url)
-    return response.json()['access_token']
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    # todo: 图片指纹库
     if 'img' not in request.files:
-        return jsonify({'error': 'No img uploaded'})
+        return jsonify({'errcode': 1, 'errmsg': 'No img uploaded'})
 
     file = request.files['img']
 
     if file.filename == '':
-        return jsonify({'error': 'No img selected'})
+        return jsonify({'errcode': 2, 'errmsg': 'No img selected'})
 
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
 
-    file.save(os.path.join(UPLOAD_FOLDER, file.filename))
-    access_token = get_token()
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    os.file.save(file_path)
     img_url = 'https://newtype.top/images/' + file.filename
-    wx_url = 'https://api.weixin.qq.com/cv/ocr/comm?access_token='+access_token+'&img_url='+img_url
-    response = requests.post(wx_url)
-    result = response.json()
-    if result['errcode'] != 0:
-        return jsonify({'result': 'upload error'})
 
-    ocr_result = ''
-    items = result['items']
-    for item in items:
-        ocr_result += item['text'] + ' '
+    ocr_result = wx.get_ocr(img_url)
+    os.remove(file_path)
 
-    return ask_chat(ocr_result)
+    ocr = ocr_result[2]
+    if ocr_result[0] != 0 or not ocr or ocr.isspace():
+        return jsonify({'errcode': ocr_result[0], 'errmsg': ocr_result[1], 'data': jsonify({'ocr': ocr_result[2]})})
 
+    result = gpt.ask(ocr)
+    return jsonify({'errcode': 0, 'errmsg': '', 'data': jsonify({'ocr': result})})
 
-def ask_chat(ingredients):
-    OPENAI_API_KEY = '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'
-    #return """提取上面文字中的食品配料表，并分析每种配料对人体是否健康，并给出食用建议\n
-    prompt =  '提取下面文字中的食品配料表，并分析每种配料对人体是否健康，并给出食用建议: {}'.format(ingredients)
-    openai.api_key = OPENAI_API_KEY
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=3000,
-        n=1,
-        stop=None,
-        temperature=0.3).choices
-    response_str = ''
-    for item in response:
-        response_str += item.text
-    return response_str
-
-
-@app.route('/doocr', methods=['POST'])
-def do_ocr():
-    data = request.get_json()
-    img_base64 = data.get('imgbase64')
-    #img_base64 = request.args.get('imgbase64', '')
-
-    mini_program_ocr_url = 'https://api.weixin.qq.com/cv/ocr/general?access_token=' + '67d2ef55944c47c92010300014b711f4'
-    data = {'img_base64': img_base64}
-
-    response = requests.post(mini_program_ocr_url, json=data)
-
-    print(response.json())
-
-    #return response.json()
-    return jsonify({'result': 'OCR completed' + img_base64})
 
 if __name__ == '__main__':
     # 启动 HTTPS 服务器
-    #app.run(ssl_context='adhoc', host='0.0.0.0', port=443)
-    #app.run(host='0.0.0.0', port=5001)
+    # app.run(ssl_context='adhoc', host='0.0.0.0', port=443)
+    # app.run(host='0.0.0.0', port=5001)
     app.run()
-
